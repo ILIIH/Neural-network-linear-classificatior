d7ce8f3939334794651cca327be522aa1368a101
illiabranchuk <jointlord0@gmail.com>
2024-08-13 18:54:02 +0100

    download dataset

--- dataset.py
+++ dataset.py
@@ -1,16 +1,10 @@
-from scipy import io
 import os
+
 import numpy as np
+import scipy.io as io
 
 
 def load_data_mat(filename, max_samples, seed=42):
-    '''
-    Loads numpy arrays from .mat file
-
-    Returns:
-    X, np array (num_samples, 32, 32, 3) - images
-    y, np array of int (num_samples) - labels
-    '''
     raw = io.loadmat(filename)
     X = raw['X']  # Array of [32, 32, 3, n_samples]
     y = raw['y']  # Array of [n_samples, 1]
@@ -18,27 +12,33 @@ def load_data_mat(filename, max_samples, seed=42):
     y = y.flatten()
     # Fix up class 0 to be 0
     y[y == 10] = 0
-
+    
     np.random.seed(seed)
     samples = np.random.choice(np.arange(X.shape[0]),
                                max_samples,
                                replace=False)
     
     return X[samples].astype(np.float32), y[samples]
-    
+
+
 def load_svhn(folder, max_train, max_test):
-    '''
-    Loads SVHN dataset from file
-
-    Arguments:
-
-
-    Returns:
-    train_X, np array (num_train, 32, 32, 3) - training images
-    train_y, np array of int (num_train) - training labels
-    test_X, np array (num_test, 32, 32, 3) - test images
-    test_y, np array of int (num_test) - test labels
-    '''
     train_X, train_y = load_data_mat(os.path.join(folder, "train_32x32.mat"), max_train)
     test_X, test_y = load_data_mat(os.path.join(folder, "test_32x32.mat"), max_test)
     return train_X, train_y, test_X, test_y
+
+
+def random_split_train_val(X, y, num_val, seed=42):
+    np.random.seed(seed)
+
+    indices = np.arange(X.shape[0])
+    np.random.shuffle(indices)
+
+    train_indices = indices[:-num_val]
+    train_X = X[train_indices]
+    train_y = y[train_indices]
+
+    val_indices = indices[-num_val:]
+    val_X = X[val_indices]
+    val_y = y[val_indices]
+
+    return train_X, train_y, val_X, val_y
