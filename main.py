f8d91456de0da869fb67bdca4c139303926d9ff6
illiabranchuk <jointlord0@gmail.com>
2024-08-14 18:36:42 +0100

    added forward for FullyConnectedLayer

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
-from matplotlib import pyplot as plt
 import numpy as np
-from dataset import load_svhn
+import matplotlib.pyplot as plt
+from dataset import load_svhn, random_split_train_val
+from gradient_check import check_layer_gradient, check_layer_param_gradient, check_model_gradient
+from layers import FullyConnectedLayer, Param, ReLULayer
+from model import TwoLayerNet
+from trainer import Trainer, Dataset
+from optim import SGD, MomentumSGD
+from metrics import multiclass_accuracy
+
+def prepare_for_neural_network(train_X, test_X):
+    train_flat = train_X.reshape(train_X.shape[0], -1).astype(np.cfloat) / 255.0
+    test_flat = test_X.reshape(test_X.shape[0], -1).astype(np.cfloat) / 255.0
+    
+    mean_image = np.mean(train_flat, axis = 0)
+    train_flat -= mean_image
+    test_flat -= mean_image
+    
+    return train_flat, test_flat
+    
 
 def main():
-    train_X, train_y, test_X, test_y = load_svhn("data", max_train=1000, max_test=100)
-    print(train_X)
-    visualise_data(train_X,train_y)
-    prepare_data(train_X,train_y,test_y,test_X)
-
-def prepare_data(train_X ,train_y,test_y,test_X):
-    binary_train_mask = (train_y == 0) | (train_y == 9)
-    binary_train_X = train_X[binary_train_mask]
-    binary_train_y = train_y[binary_train_mask] == 0
-
-    binary_test_mask = (test_y == 0) | (test_y == 9)
-    binary_test_X = test_X[binary_test_mask]
-    binary_test_y = test_y[binary_test_mask] == 0
-
-    # Reshape to 1-dimensional array [num_samples, 32*32*3]
-    binary_train_X = binary_train_X.reshape(binary_train_X.shape[0], -1)
-    binary_test_X = binary_test_X.reshape(binary_test_X.shape[0], -1)
-    
-def visualise_data(train_X, train_Y):
-    samples_per_class = 5  
-    plot_index = 1
-    for example_index in range(samples_per_class):
-        for class_index in range(10):
-            plt.subplot(5, 10, plot_index)
-            image = train_X[train_Y == class_index][example_index]
-            plt.imshow(image.astype(np.uint8))
-            plt.axis('off')
-            plot_index += 1
+    train_X, train_y, test_X, test_y = load_svhn("data", max_train=10000, max_test=1000)    
+    train_X, test_X = prepare_for_neural_network(train_X, test_X)
+    train_X, train_y, val_X, val_y = random_split_train_val(train_X, train_y, num_val = 1000)
+    print(0.001 * np.random.randn(3, 3))
 
 if __name__ == "__main__":
     main()
